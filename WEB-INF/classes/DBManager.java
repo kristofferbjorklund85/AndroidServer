71a59202e5f7f72c7b320ccda42cf6689880186f
Pregmark <sam.pregmark@gmail.com>
2017-11-16 14:27:38 +0100

    Added commit, rollback, create Resultset and readfromDB methods in DBManager

--- WEB-INF/classes/DBManager.java
+++ WEB-INF/classes/DBManager.java
@@ -1,3 +1,9 @@
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBManager {
@@ -6,8 +12,64 @@ public class DBManager {
 
     }
 
-    public static void readFromDb(List list) {
+    public static List readFromDb() {
+        List<CampsiteModel> campList = new ArrayList<>();
 
+        if(DBServlet.hasConnection()) {
+            String query = "SELECT * FROM campsites";
+
+            ResultSet rs = createRS(query);
+            System.out.println("Campsites retrieved");
+
+            try {
+                while(rs.next()) {
+                    CampsiteModel cm = new CampsiteModel(   rs.getInt(1),
+                                                            rs.getString(2),
+                                                            rs.getString(3),
+                                                            rs.getString(4),
+                                                            rs.getString(5),
+                                                            rs.getInt(6),
+                                                            rs.getString(6),
+                                                            rs.getString(7));
+                    campList.add(cm);
+                }
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("Returning campList");
+        return campList;
+    }
+
+    public static ResultSet createRS(String query) {
+        Connection c = DBServlet.getConnection();
+        Statement s = null;
+        ResultSet rs = null;
+
+        try {
+            s = c.createStatement();
+            rs = s.executeQuery(query);
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+        return rs;
+    }
+
+    public static void commitSQL(Connection c) {
+        try {
+            c.commit();
+        } catch (SQLException e) {
+            rollbackSQL(c);
+        }
+    }
+
+    public static void rollbackSQL(Connection c) {
+        try {
+            c.rollback();
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
     }
 
 }
