2d0cb15ef2e49d27a837bd2f6c4c35a4efc5bc25
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-16 15:10:15 +0100

    Expanded seed-method, added test-methods to main

--- WEB-INF/classes/Main.java
+++ WEB-INF/classes/Main.java
@@ -1,15 +1,19 @@
+import org.json.JSONArray;
 
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Running db.Main...");
-        if(DBServlet.hasConnection()) {
-            System.out.println("We have a connection");
-        } else {
-            System.out.println("There was a problem getting a connection.");
-        }
+
         Seed.init();
+
+        List list = DBManager.readFromDb();
+        JSONArray jRay = JSONManager.toJSON(list);
+        List jList = JSONManager.fromJSON(jRay);
+
+        System.out.println("Finished");
     }
 }
 
--- WEB-INF/classes/Seed.java
+++ WEB-INF/classes/Seed.java
@@ -9,54 +9,56 @@ public class Seed {
     }
 
     private static void deleteTables() {
-        if(DBServlet.hasConnection()) {
-            String sql = "DROP TABLE IF EXISTS campsites";
-            try {
-                Statement stmt = DBServlet.getConnection().createStatement();
-                // create a new table
-                stmt.execute(sql);
-                System.out.println("Dropped table");
-            } catch(SQLException e) {
-                System.out.println(e.getMessage());
-            }
+        String sql = "DROP TABLE IF EXISTS campsites";
+        try {
+            Statement stmt = DBServlet.getConnection().createStatement();
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Dropped table");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     private static void createNewTable() {
-        if(DBServlet.hasConnection()) {
-            // SQL statement for creating a new table
-            String sql = "CREATE TABLE IF NOT EXISTS campsites (\n"
-                    + "	id integer PRIMARY KEY,\n"
-                    + "	location text NOT NULL,\n"
-                    + "	coordinates text,\n"
-                    + "	type text,\n"
-                    + "	fee text,\n"
-                    + "	capacity text,\n"
-                    + "	availability text,\n"
-                    + "	description text\n"
-                    + ");";
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS campsites (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	location text NOT NULL,\n"
+                + "	coordinates text,\n"
+                + "	type text,\n"
+                + "	fee text,\n"
+                + "	capacity text,\n"
+                + "	availability text,\n"
+                + "	description text\n"
+                + ");";
 
-            try {
-                Statement stmt = DBServlet.getConnection().createStatement();
-                // create a new table
-                stmt.execute(sql);
-                System.out.println("Table created");
-            } catch(SQLException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            Statement stmt = DBServlet.getConnection().createStatement();
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Table created");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     private static void seedDB() {
-        if(DBServlet.hasConnection()) {
-            try {
-                String sql = "INSERT INTO campsites VALUES('1','Lindholmen','44,34', 'School', 'Free', '30', 'All year', 'Very nice place, lots of cool people')";
-                Statement stmt = DBServlet.getConnection().createStatement();
-                stmt.executeUpdate(sql);
-                System.out.println("Database seeded!");
-            } catch(SQLException e) {
-                System.out.println(e.getMessage());
+        try {
+            Statement stmt = DBServlet.getConnection().createStatement();
+
+            String[] sql = {    "INSERT INTO campsites VALUES('1','Lindholmen','44,34', 'School', 'Free', '30', 'All year', 'Very nice place, lots of cool people')",
+                                "INSERT INTO campsites VALUES('2','Lindholmen','44,34', 'School', 'Free', '30', 'All year', 'Very nice place, lots of cool people')",
+                                "INSERT INTO campsites VALUES('3','Lindholmen','44,34', 'School', 'Free', '30', 'All year', 'Very nice place, lots of cool people')"};
+
+            for (int i = 0; i < sql.length; i++) {
+                stmt.executeUpdate(sql[i]);
             }
+
+            DBManager.commitSQL(stmt);
+            System.out.println("Database seeded!");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
