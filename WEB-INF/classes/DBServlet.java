a0740b8963a3b666c9d8e85fe4ee7dc7a47be92a
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-17 12:44:47 +0100

    Changed a lot of settings to compiler, created a temp GET-method for servlet

--- WEB-INF/classes/DBServlet.java
+++ WEB-INF/classes/DBServlet.java
@@ -1,13 +1,22 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class DBServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
-
+        PrintWriter out =
+                new PrintWriter(new OutputStreamWriter(resp.getOutputStream(),
+                        UTF_8), true);
+        out.println("<html><head><title>Example servlet</title></head>");
+        out.println("<body><p>Hello from a servlet!</body>");
+        out.println("</html>");
+        out.close();
 
         resp.setStatus(200);
     }
