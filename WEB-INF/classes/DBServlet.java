f9c60eeedb70891eb85ac8a0954c4ba86dd5c0d9
kristofferbjorklund85 <kristofferbjorklund85@gmail.com>
2017-11-17 14:21:47 +0100

    finished GET-method

--- WEB-INF/classes/DBServlet.java
+++ WEB-INF/classes/DBServlet.java
@@ -1,23 +1,33 @@
+import org.json.JSONArray;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.List;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class DBServlet extends HttpServlet {
 
+    public void init() throws ServletException  {
+        DBConMan.createConnection();
+        //Seed.init();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
-        PrintWriter out =
-                new PrintWriter(new OutputStreamWriter(resp.getOutputStream(),
-                        UTF_8), true);
-        out.println("<html><head><title>Example servlet</title></head>");
-        out.println("<body><p>Hello from a servlet!</body>");
-        out.println("</html>");
-        out.close();
+        resp.setContentType("application/json");
 
+        List list = DBManager.readFromDb();
+        JSONArray jRay = JSONManager.toJSON(list);
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(jRay);
+        out.close();
         resp.setStatus(200);
     }
 
--- WEB-INF/classes/Main.java
+++ WEB-INF/classes/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Running db.Main...");
 
-        DBConMan.createConnection();
-        //Seed.init();
+        /*DBConMan.createConnection();
+        Seed.init();
 
         List list = DBManager.readFromDb();
         JSONArray jRay = JSONManager.toJSON(list);
         List jList = JSONManager.fromJSON(jRay);
 
-        System.out.println("Finished");
+        System.out.println("Finished");*/
     }
 }
 
